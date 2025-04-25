9cf8751900526b2bf0ae2d05b0a0c5da59fbb494
tariroMashCasa <ucfntma@ucl.ac.uk>
2025-04-25 09:28:26 +0100

    Add files via upload

--- Home.py
+++ Home.py
@@ -291,11 +291,14 @@ with col2_original:
                     st.session_state.gdf_boroughs = gdf_boroughs
             # st.write(f"{gdf_boroughs.columns}")
             # [['lad22nm', 'geometry']]  # select only name + geometry
-            
-                # calculate the midpoint of london
-                london_midpoint_latitude, london_midpoint_longitude = gdf_boroughs.to_crs(4326).geometry.centroid.y.mean(), gdf_boroughs.to_crs(4326).geometry.centroid.x.mean()
-                st.session_state.london_midpoint_latitude = london_midpoint_latitude
-                st.session_state.london_midpoint_longitude = london_midpoint_longitude
+                if st.session_state.london_midpoint_latitude is None:
+                    # calculate the midpoint of london
+                    london_midpoint_latitude, london_midpoint_longitude = gdf_boroughs.to_crs(4326).geometry.centroid.y.mean(), gdf_boroughs.to_crs(4326).geometry.centroid.x.mean()
+                    st.session_state.london_midpoint_latitude = london_midpoint_latitude
+                    st.session_state.london_midpoint_longitude = london_midpoint_longitude
+                else:
+                    london_midpoint_latitude = st.session_state.london_midpoint_latitude
+                    london_midpoint_longitude = st.session_state.london_midpoint_longitude
             # st.write(f"London midpoint: {london_midpoint_latitude}, {london_midpoint_longitude}")
 
             if st.session_state.gdf_results is None:
